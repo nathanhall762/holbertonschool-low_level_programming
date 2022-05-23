4e97cc4c2c46e699ce54f070e25ecb6206132043
nathanhall762 <4943@holbertonschool.com>
2022-05-23 08:22:28 -0700

    task 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,18 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * srand - random
+ *
+ * rand - random
+ *
+ * printf - Print string
+ *
+ * Return: always 0
+ */
+
 int main(void)
 {
 	int n;

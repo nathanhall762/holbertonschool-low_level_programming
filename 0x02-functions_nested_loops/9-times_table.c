4830ec96743a53cbb5aaa812e970b16d534ca9da
nathanhall762 <4943@holbertonschool.com>
2022-05-24 15:03:27 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,24 +1,38 @@
 #include "main.h"
 
-/*
-*
+/**
+* times_table - makes Nathan angry
 */
 
 void times_table(void)
 {
 	int x = 0;
 	int y = 0;
+	int z;
 
 	for (y = 0; y <= 9; y++)
 	{
-		_putchar((x * y) + 48);
-		if (y < 9)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-		}
-		else
-			_putchar('\n');
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+			for (x = 1; x <= 9; x++)
+			{
+				z = (x * y);
+				if ((z / 10) > 0)
+				{
+					_putchar((z / 10) + '0');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+				_putchar((z % 10) + '0');
+				if (x < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		_putchar('\n');
 	}
 }

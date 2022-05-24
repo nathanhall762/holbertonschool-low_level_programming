7946515a29c0621868b845764b5fcec184fe6ea9
nathanhall762 <4943@holbertonschool.com>
2022-05-24 10:53:32 -0700

    edit task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,20 +12,11 @@ void print_alphabet_x10(void)
 	char c = 'a';
 	int n = 0;
 
-	for (n = 0; n <= 10; n++)
+	for (n = 0; n < 10; n++)
 	{
 		for (; c <= 'z'; c++)
-		{
 			_putchar(c);
-			if (n <= 10)
-			{
-				if (c == 'z')
-				{
-					_putchar('\n');
-					((c = '`'));
-					(n++);
-				}
-			}
-		}
+		_putchar('\n');
+		((c = 'a'));
 	}
 }

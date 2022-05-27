0373a0e4c2373d6d238fb4e432c2baf3707c3bb7
nathanhall762 <4943@holbertonschool.com>
2022-05-27 09:55:27 -0700

    midway in task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,10 +1,29 @@
 #include "main.h"
 
 /**
+* more_numbers - prints the numbers, from 0 to 14, 10 times followed by a new line
 *
+* Return: void
 */
 
-
+void more_numbers(void)
 {
-	
+	int c;
+	int lines;
+
+	for (lines = 0; lines <= 10; lines++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c <= 9)
+			{
+				_putchar(c + '0');
+			}
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }

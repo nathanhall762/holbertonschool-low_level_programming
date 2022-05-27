a7111e4fe96d4934e6c69d5c11d174d00c104b5e
nathanhall762 <4943@holbertonschool.com>
2022-05-27 08:59:00 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
 /**
+* _isupper - finds if int c is upper case or not
 *
+* Return: 1 if upper, 0 if else
 */
 
 int _isupper(int c)
 {
-	
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
 }

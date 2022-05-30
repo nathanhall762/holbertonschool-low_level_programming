c4752a199d703f8e543f1851ce0e17eba824e295
nathanhall762 <4943@holbertonschool.com>
2022-05-30 06:04:55 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,10 +1,20 @@
 #include "main.h"
 
 /**
-*
+* print_line - prints line of n length
+* @n: line length
 */
 
-
+void print_line(int n)
 {
-	
+	int x;
+
+	if (n > 0)
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
 }

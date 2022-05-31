f367ffe459c8ca750363c0a5a9f6257e304978c4
nathanhall762 <4943@holbertonschool.com>
2022-05-31 05:28:43 -0700

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,10 +1,28 @@
 #include "main.h"
 
 /**
-*
+* print_square - prints a squar of n size
+* @size: size of block
 */
 
-
+void print_square(int size)
 {
-	
+	int x;
+	int y;
+
+	if (size > 0)
+	{
+		for (y = 0; y < size; y++)
+		{
+			for (x = 0; x < size; x++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }

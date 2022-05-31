a5d6674470acb57af7feb6bcec71639016b533c0
nathanhall762 <4943@holbertonschool.com>
2022-05-31 08:14:46 -0700

    moved task 9 to .

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,10 +1,28 @@
-#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
+* main - entry point
 *
+* Return: 0
 */
 
-
+int main(void)
 {
-	
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 == 0)
+			printf("Fizz");
+		else if (x % 5 == 0)
+			printf("Buzz");
+		else if (x % 5 == 0 && x % 3 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", x);
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
 }

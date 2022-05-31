0e2cca213cd4fef8891466281795113e3a222b4a
nathanhall762 <4943@holbertonschool.com>
2022-05-31 08:24:02 -0700

    moved fizbuzz to if

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,15 +13,17 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if (x % 3 == 0)
+		if ((x % 5 == 0) && (x % 3 == 0))
+			printf("FizzBuzz");
+		else if (x % 3 == 0)
 			printf("Fizz");
 		else if (x % 5 == 0)
 			printf("Buzz");
-		else if (x % 5 == 0 && x % 3 == 0)
-			printf("FizzBuzz");
 		else
 			printf("%d", x);
-		printf(" ");
+
+		if (x != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);

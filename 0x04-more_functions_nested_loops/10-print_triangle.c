ccb18c15896e93b0169941cfae3bcce94775a0ab
nathanhall762 <4943@holbertonschool.com>
2022-05-31 05:55:36 -0700

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,10 +1,37 @@
 #include "main.h"
 
 /**
-*
+* print_triangle - prints a triangle of n size
+* @size: size of triangle
 */
 
-
+void print_triangle(int size)
 {
-	
+	int x;
+	int y;
+	int spacecount;
+
+	if (size > 0)
+	{
+		for (y = 1; y <= size; y++)
+		{
+			spacecount = (size - y);
+			for (x = 0; x < size; x++)
+			{
+				if (x < spacecount)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }

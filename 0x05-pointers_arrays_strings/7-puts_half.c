3562f84f892bafab9b2989a3514f92f0f104e6d2
nathanhall762 <4943@holbertonschool.com>
2022-06-01 12:31:12 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,10 +1,21 @@
 #include "main.h"
 
 /**
-*
+* puts_half - prints half of a string followed by a new line to stdout
+* @str: string
 */
 
-
+void puts_half(char *str)
 {
-	
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+	}
+	len = len / 2;
+	for (; str[len] != '\0'; len++)
+	{
+		_putchar(str[len]);
+	}
+	_putchar('\n');
 }

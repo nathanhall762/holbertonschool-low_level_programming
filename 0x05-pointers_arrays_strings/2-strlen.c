723eb9672e25bc555dc5c0870cba05d0ab0231dc
nathanhall762 <4943@holbertonschool.com>
2022-06-01 09:50:01 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
 /**
-*
+* _strlen - return the lenth of a string
+* @s: string
+* Return: length of string
 */
 
-
+int _strlen(char *s)
 {
-	
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	return (len);
 }

29a8e23cea81ab1ef38b4690a69c78126dacadfc
nathanhall762 <4943@holbertonschool.com>
2022-06-01 10:01:11 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -12,4 +12,4 @@ int main(void)
     str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
     _puts(str);
     return (0);
-
+}
--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
 /**
-*
+* _puts - prints a string followed by a new line to stdout
+* @str: string
 */
 
-
+void _puts(char *str)
 {
-	
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+		_putchar(str[len]);
+	}
+	_putchar('\n');
 }

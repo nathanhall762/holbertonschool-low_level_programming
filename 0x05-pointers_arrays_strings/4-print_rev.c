3c2a6c3cef0aa1dbbacafba0d9746e738aac8de7
nathanhall762 <4943@holbertonschool.com>
2022-06-01 11:29:45 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,20 @@
 #include "main.h"
 
 /**
-*
+* print_rev - prints a string in reverse followed by a new line to stdout
+* @s: string
 */
 
-
+void print_rev(char *s)
 {
-	
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	for (; len >= 0; len--)
+	{
+		_putchar(s[len]);
+	}
+	_putchar('\n');
 }

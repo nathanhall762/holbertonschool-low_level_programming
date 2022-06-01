93a1e3a65d322d64259ce7122b7049d7cdd6d890
nathanhall762 <4943@holbertonschool.com>
2022-06-01 12:04:44 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,25 @@
 #include "main.h"
 
 /**
-*
+* rev_string - reverses a string
+* @s: string
 */
 
-
+void rev_string(char *s)
 {
-	
+	int len;
+	int right, left, i, tmp;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	left = 0;
+	right = len - 1;
+	for (i = left; i < right; i++)
+	{
+		tmp = s[i];
+		s[i] = s[right];
+		s[right] = tmp;
+		right--;
+	}
 }

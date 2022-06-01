ac71405e9024fbc7c6d63de3b68ce038fd7c75c0
nathanhall762 <4943@holbertonschool.com>
2022-06-01 12:14:17 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,10 +1,18 @@
 #include "main.h"
 
 /**
-*
+* puts2 - prints every other char of a string followed by a new line to stdout
+* @str: string
 */
 
-
+void puts2(char *str)
 {
-	
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+		_putchar(str[len]);
+		len++;
+	}
+	_putchar('\n');
 }

d199a185d51f1c5bea28f4602d8143ff4746d8a1
nathanhall762 <4943@holbertonschool.com>
2022-06-06 12:36:00 -0700

    start task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,9 +1,32 @@
 #include "holberton.h"
 
 /**
-*
+* cap_string - capitalizes all words of a string
+* @str: string
+* Return: char
 */
 
-
+char *cap_string(char *str)
 {
+	int i, cap, sepx;
+	int separators[] = {',', ';', '.', '?', '"', '(', ')', '{', '}', ' ', '\n', '\t'};
+
+	cap = 32;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - cap;
+		}
+		cap = 0;
+		for (sepx = 0; sepx < 12; sepx++)
+		{
+			if (str[sepx] == separators[sepx])
+				{
+					cap = 32;
+					sepx = 12;
+				}
+		}
+	}
+	return (str);
 }

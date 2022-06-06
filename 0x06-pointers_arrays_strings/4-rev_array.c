0f1c35cddb4e3f5e367858e8397de93e06f7f4c4
nathanhall762 <4943@holbertonschool.com>
2022-06-06 11:02:05 -0700

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,9 +1,23 @@
 #include "holberton.h"
 
 /**
-*
+* reverse_array - reverses the content of an array of integers
+* @a: array
+* @n: sizeof
 */
 
-
+void reverse_array(int *a, int n)
 {
+	int len = n;
+	int right, left, i, tmp;
+
+	left = 0;
+	right = len - 1;
+	for (i = left; i < right; i++)
+	{
+		tmp = a[i];
+		a[i] = a[right];
+		a[right] = tmp;
+		right--;
+	}
 }

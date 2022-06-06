c7d2dd8d30e46fcf47bcb0c75b1b6de600af5a4d
nathanhall762 <4943@holbertonschool.com>
2022-06-06 09:57:32 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,16 @@
 #include "holberton.h"
 
 /**
+* _strcmp - compares two strings
+* @s1: string 1
+* @s2: string 2
 *
+* Return: integer
 */
 
-
+int _strcmp(char *s1, char *s2)
 {
+	int compare = (s1[0] - s2[0]);
+
+	return (compare);
 }

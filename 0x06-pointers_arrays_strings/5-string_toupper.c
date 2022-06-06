07b963a260e8da6d3bb1e328c9a65adb90510c0f
nathanhall762 <4943@holbertonschool.com>
2022-06-06 11:15:05 -0700

    tasl 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,21 @@
 #include "holberton.h"
 
 /**
-*
+* string_toupper - changes all lowercase letters of a string to uppercase
+* @str: string
+* Return: char
 */
 
-
+char *string_toupper(char *str)
 {
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = (str[i] - 32);
+		}
+	}
+	return (str);
 }

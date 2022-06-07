1e104582818bd17c12e80724ffb3d0335ea36b89
nathanhall762 <4943@holbertonschool.com>
2022-06-07 10:12:14 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,28 @@
 #include "main.h"
 
 /**
+* _strchr - locates a character in a string
+* @s: first string
+* @c: character to locate
 *
+* Return: first occurence of c
 */
 
-
+char *_strchr(char *s, char c)
 {
-	
+	int i;
+	char *ptr;
+
+	for (i = 0; s[i] != c && s[i] != '\0'; i++)
+	{
+	}
+	ptr = &s[i];
+	if (s[i] == '\0')
+	{
+		return ('\0');
+	}
+	else
+	{
+		return (ptr);
+	}
 }
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,10 +1,28 @@
 #include "main.h"
 
 /**
+* _strspn - gets the length of a prefix substring
+* @s: first string
+* @accept: second string
 *
+* Return: number of bytes 
 */
 
-
+unsigned int _strspn(char *s, char *accept)
 {
-	
+	int i, j, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		j = 0;
+		while (accept[j])
+		{
+			if (s[i] == accept[j])
+				n++;
+			else
+				break;
+			j++;
+		}
+	}
+	return (n);
 }

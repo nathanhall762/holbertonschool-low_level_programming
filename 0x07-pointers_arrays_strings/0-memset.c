31a2d731f970eda4a9cc9efaae3af2064b0423f2
nathanhall762 <4943@holbertonschool.com>
2022-06-07 09:01:31 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,21 @@
 #include "main.h"
 
 /**
+* _memset - fills memory with a constant byte
+* @s: array
+* @b: new char
+* @n: number to replace
 *
+* Return: s
 */
 
-
+char *_memset(char *s, char b, unsigned int n)
 {
-	
+	int i;
+
+	for (i = 0; i < (int) n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }

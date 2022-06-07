cfd97d7af7926a9a2afbfce57e0b554539715877
nathanhall762 <4943@holbertonschool.com>
2022-06-07 11:57:15 -0700

    change task 2 to return 0

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,10 +1,22 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-*
+* print_diagsums - prints the sum of the two diagonals of sqr matrix
+* @a: array
+* @size: size of array
 */
 
-
+void print_diagsums(int *a, int size)
 {
-	
+	int sum1, i, j;
+
+	sum1 = 0;
+	j = 0;
+	for (i = 0; i < size; i++)
+		{
+			sum1 = sum1 + a[i][j];
+			j++;
+		}
+	printf("%d", sum1);
 }

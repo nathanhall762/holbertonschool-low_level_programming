a80de88cee29a183b074a05e31a8a3d164bb7d0b
nathanhall762 <4943@holbertonschool.com>
2022-06-07 18:41:23 -0700

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,10 +1,36 @@
 #include "main.h"
 
 /**
+* _strstr - locats a substring
+* @haystack: string 1
+* @needle: string 2
 *
+* Return: pointer or NULL
 */
 
-
+char *_strstr(char *haystack, char *needle)
 {
-	
+	int i = 0, j = 0;
+
+	while (haystack[i])
+	{
+		while (needle[j])
+		{
+			if (haystack[i + j] != needle[j])
+			{
+				break;
+			}
+
+			j++;
+		}
+
+		if (needle[j] == '\0')
+		{
+			return (haystack + i);
+		}
+
+		i++;
+	}
+
+	return (0);
 }

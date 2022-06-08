050c53e6f06c944d45ae87ac260b160d3209165a
nathanhall762 <4943@holbertonschool.com>
2022-06-08 15:10:06 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,30 @@
 #include "main.h"
+int squareroot(int n, int i);
+/**
+* _sqrt_recursion - sends n to squareroot function recursively
+* @n: integer
+* Return: 0
+*/
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (squareroot(n, (n + 1) / 2));
+}
 
 /**
-*
+* squareroot - checks if square
+* @n: integer
+* @i: integer
+* Return: integer
 */
-
-
+int squareroot(int n, int i)
 {
-	
+	if (i < 1)
+		return (-1);
+	else if (i * i == n)
+		return (i);
+	else
+		return (squareroot(n, i - 1));
 }

1d1af78e7103600c48fd7c51f6a74a9fef559bc5
nathanhall762 <4943@holbertonschool.com>
2022-06-07 18:29:09 -0700

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,10 +1,29 @@
 #include "main.h"
 
 /**
+* _strpbrk - searches a string for any set of a set of bytes
+* @s: first string
+* @accept: second string
 *
+* Return: s or NULL
 */
 
-
+char *_strpbrk(char *s, char *accept)
 {
-	
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		j = 0;
+		while (accept[j])
+		{
+			if (s[i] == accept[j])
+			{
+				s += i;
+				return (s);
+			}
+			j++;
+		}
+	}
+	return (s);
 }

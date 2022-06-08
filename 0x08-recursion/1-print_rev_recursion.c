f14037cffe850e3f5cbf0991778cad73a4e8ae34
nathanhall762 <4943@holbertonschool.com>
2022-06-08 08:20:21 -0700

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,10 +1,15 @@
 #include "main.h"
 
 /**
-*
+* _print_rev_recursion - like puts in reverse, but using recursion
+* @s: string
 */
 
-
+void _print_rev_recursion(char *s)
 {
-	
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }

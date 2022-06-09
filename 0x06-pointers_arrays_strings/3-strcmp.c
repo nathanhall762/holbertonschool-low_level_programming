d88485704b6f46c9c0f07ea47322ebdd61f9bd3a
nathanhall762 <4943@holbertonschool.com>
2022-06-09 10:51:58 -0700

    betty

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,9 +12,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	if (s1[i] != s2[i])
-		return (s1[i] - s2[i]);
-	i++;
+	while (*s1)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
 
 	return (s1[i] - s2[i]);
 }

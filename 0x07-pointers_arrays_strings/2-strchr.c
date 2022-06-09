0c9a767f21b3f13497aab66c311a403eb2f414ce
nathanhall762 <4943@holbertonschool.com>
2022-06-09 16:37:38 -0700

    edit task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,18 +11,11 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	char *ptr;
-
-	for (i = 0; s[i] != c && s[i] != '\0'; i++)
+	
+	for (i = 0; s[i] >= '\0'; i++)
 	{
+		if (s[i] == c)
+			return (s + i);
 	}
-	ptr = &s[i];
-	if (s[i] == '\0')
-	{
-		return ('\0');
-	}
-	else
-	{
-		return (ptr);
-	}
+	return (0);
 }

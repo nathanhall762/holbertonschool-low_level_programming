e4277c366db31c7d4c8b06641cd8820db0f94ef1
nathanhall762 <4943@holbertonschool.com>
2022-06-09 16:30:00 -0700

    edit task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,9 +10,16 @@
 char *leet(char *s)
 {
 	int i, j;
+	char old[] = "aAeEoOtTlL";
+	char new[] = "4433007711";
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; *(s + i); i++)
 	{
-		
+		for (j = 0; j <= 9; j++)
+		{
+			if (old[j] == *(s + i))
+				*(s + i) = new[j];
+		}
 	}
+	return (s);
 }

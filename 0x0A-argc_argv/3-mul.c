620edd610acfdc67090ea26f2a1986457ee44709
nathanhall762 <4943@holbertonschool.com>
2022-06-14 10:16:06 -0700

    changed return order

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -18,11 +18,11 @@ int main(int argc, char *argv[])
 	{
 		result = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", result);
+		return (0);
 	}
 	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	return (0);
 }

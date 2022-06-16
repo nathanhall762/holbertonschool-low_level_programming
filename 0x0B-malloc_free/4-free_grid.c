7893f979f0eecfe7ee28ac6d1dd3d40a93b7357e
nathanhall762 <4943@holbertonschool.com>
2022-06-16 13:50:24 -0700

    test: checker task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,10 +1,22 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-*
+* free_grid - frees a 2 dim grid
+* @grid: pointer to pointer grid
+* @height: height of grid
 */
 
-
+void free_grid(int **grid, int height)
 {
-	
+	int i;
+
+	if (grid != NULL)
+	{
+		for (i = 0; i < height; i++)
+		{
+			free(grid[i]);
+		}
+	}
 }
---
+++ 0x0B-malloc_free/4-main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int **alloc_grid(int, int);
+void free_grid(int **, int);
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int **grid;
+	int h;
+	int w;
+
+	h = 10;
+	w = 20;
+	grid = alloc_grid(w, h);
+	if (grid == NULL)
+	{
+		return (1);
+	}
+	free_grid(grid, h);
+	return (0);
+}

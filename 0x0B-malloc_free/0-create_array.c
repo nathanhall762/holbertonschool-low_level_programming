5cd3012139d6b67aebae66f3bcf6ab4bd9234e8f
nathanhall762 <4943@holbertonschool.com>
2022-06-16 11:23:05 -0700

    fix: rewrote 0-main.c and main.h

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 
 	array = malloc((sizeof(char) * size));
 
-	if ((size == 0 || array = NULL))
+	if ((size == 0 || array == NULL))
 	{
 		return (NULL);
 	}

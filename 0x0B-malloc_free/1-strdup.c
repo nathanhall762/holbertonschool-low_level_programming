6836c6f43af4b286d866be813befcc7bf4077463
nathanhall762 <4943@holbertonschool.com>
2022-06-16 11:43:06 -0700

    test: confirm github push

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,10 +1,32 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
+* _strdup - returns a pointer to a new memory space with string parameter
+* @size: size of array
+* @c: character
 *
+* Return: NULL if array write fails or size is 0, else array
 */
 
-
+char *_strdup(char *str)
 {
-	
+	char *array;
+	int i;
+
+	if ((str == NULL))
+	{
+		return (NULL);
+	}
+	while (*str)
+	{
+		array[i] = str[i];
+		i++;
+	}
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	return (array);
 }

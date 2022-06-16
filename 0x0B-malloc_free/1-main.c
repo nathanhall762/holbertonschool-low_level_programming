97b8f55f0bbef309a9765d8991e5dc9dbb90cb5c
nathanhall762 <4943@holbertonschool.com>
2022-06-16 11:58:52 -0700

    fix: task 1 rework

--- 0x0B-malloc_free/1-main.c
+++ 0x0B-malloc_free/1-main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char *_strdup(char *str);
+
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char *s;
+	char *s;
 
-    s = _strdup("Duplicated");
-    if (s == NULL)
-    {
-        printf("failed to allocate memory\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    return (0);
+	s = _strdup("Holberton School!");
+	if (s == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+	return (0);
 }
--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,8 +4,7 @@
 
 /**
 * _strdup - returns a pointer to a new memory space with string parameter
-* @size: size of array
-* @c: character
+* @str: string
 *
 * Return: NULL if array write fails or size is 0, else array
 */
@@ -13,20 +12,25 @@
 char *_strdup(char *str)
 {
 	char *array;
-	int i;
+	int i, last;
 
-	if ((str == NULL))
+	if (str == NULL)
 	{
 		return (NULL);
 	}
-	while (*str)
+	for (last = 0; last <= *str; last++)
+	{
+	}
+	last += 1;
+	array = malloc(sizeof(char) * last);
+	for (i = 0; i <= last; i++)
 	{
 		array[i] = str[i];
-		i++;
 	}
 	if (array == NULL)
 	{
 		return (NULL);
 	}
+
 	return (array);
 }

bfdd8d1eb8734a04f29aaf24976c15ee588f0b1d
nathanhall762 <4943@holbertonschool.com>
2022-06-20 08:46:31 -0700

    fix: null to nothing

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,9 +18,9 @@ char *str_concat(char *s1, char *s2)
 	char *a;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 	a = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
 	if (a == NULL)
 		return (NULL);

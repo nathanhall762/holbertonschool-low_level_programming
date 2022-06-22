6c294500253ee060b8e14e9628c8d158c7cea2ae
nathanhall762 <4943@holbertonschool.com>
2022-06-22 10:04:09 -0700

    fix: task 2 compile

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,9 +3,14 @@
 #include <stdlib.h>
 
 /**
+* new_dog - creates a new dog
+* @name: dog name
+* @age: dog age
+* @owner: dog owner
 *
+* Return: Null if function fails, else pointer to dog_t
 */
 
-{
-	
+dog_t *new_dog(char *name, float age, char *owner)
+{	
 }

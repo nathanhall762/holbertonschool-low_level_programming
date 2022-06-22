c0dce32ca29036dc3c1b2d6c00ada6ee1f00375f
nathanhall762 <4943@holbertonschool.com>
2022-06-22 09:10:45 -0700

    init: task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - dog data
+* @name: name of dog
+* @age: age of dog
+* @owner: dog's owner
+*
+* Description: dog data
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

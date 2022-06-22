d7f8e71cc91fe04e36d5c6aac65354ac7aa6feb9
nathanhall762 <4943@holbertonschool.com>
2022-06-22 10:14:28 -0700

    init: task 5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,5 +12,10 @@
 */
 
 dog_t *new_dog(char *name, float age, char *owner)
-{	
+{
+	dog_t *newdog;
+
+	if (name != NULL && age >= 0 && owner != NULL)
+		return (newdog);
+	return (newdog);
 }
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -3,9 +3,16 @@
 #include <stdlib.h>
 
 /**
-*
+* free_dog - frees dogs
+* @d: address of struct
 */
 
+void free_dog(dog_t *d)
 {
-	
+	if (d != NULL)
+	{
+		free((*d).owner);
+		free((*d).name);
+		free((*d);
+	}
 }

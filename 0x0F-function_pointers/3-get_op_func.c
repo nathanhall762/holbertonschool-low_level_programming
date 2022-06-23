b9cb2fbee2203525c2091f51cb5adb28cf66f6c7
nathanhall762 <4943@holbertonschool.com>
2022-06-23 12:31:57 -0700

    save: task 3 progress

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,9 +1,29 @@
 #include "function_pointers.h"
 
 /**
+* get_op_func - sends integers to proper function
+* @s: operand
 *
+* Return: pointer to corresponding function
 */
 
-
+int (*get_op_func(char *s))(int, int)
 {
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (*(ops[i].op) == *s)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,9 +1,29 @@
 #include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
 *
 */
 
-
+int main(int argc, char *argv[])
 {
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	(*get_op_func)(argv[3]);
+
+/*
+	if
+	{
+		
+	}
+	if
+	{
+		
+	}
+*/
+	return (0);
 }

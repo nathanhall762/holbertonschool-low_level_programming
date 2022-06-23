fa7f307e425d982a97f11d86b5afe2ee358e1d6e
nathanhall762 <4943@holbertonschool.com>
2022-06-23 10:45:53 -0700

    init: task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,9 +1,67 @@
-#include "function_pointers.h"
+#include "3-calc.h"
 
 /**
+* op_add - adds 2 ints
+* @a: int 1
+* @b: int 2
 *
+* Return: a+b
 */
 
-
+int op_add(int a, int b)
 {
+	return (a + b);
 }
+
+/**
+* op_sub - subtract 2 ints
+* @a: int 1
+* @b: int 2
+*
+* Return: a-b
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - multiply 2 ints
+* @a: int 1
+* @b: int 2
+*
+* Return: a*b
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - div 2 ints
+* @a: int 1
+* @b: int 2
+*
+* Return: a/b
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - modulos 2 ints
+* @a: int 1
+* @b: int 2
+*
+* Return: a%b
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+

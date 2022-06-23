a3b78b843f13f8191961954b7b044aa948dd7af1
nathanhall762 <4943@holbertonschool.com>
2022-06-23 10:08:55 -0700

    init: task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,9 +1,32 @@
 #include "function_pointers.h"
 
 /**
+* int_index - finds first index of matching int
+* @array: array
+* @size: size of array
+* @cmp: function pointer
 *
+* Return: -1 if no match, else first of int match
 */
 
-
+int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+	if (array == NULL)
+		return (-1);
+	if (cmp == NULL)
+		return (-1);
+
+	while (i < size)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+		i++;
+	}
+	return (-1);
 }

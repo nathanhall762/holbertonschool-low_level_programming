1c14a89b2d2323ef25f2898462b7019868ca8a50
nathanhall762 <4943@holbertonschool.com>
2022-06-30 13:01:25 -0700

    fix: moved NULL conditional

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,16 +13,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list lst;
 	unsigned int i;
 
-	if (separator == NULL)
-		return;
-
 	va_start(lst, n);
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(lst, const unsigned int));
 
-		if (i != (n - 1))
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");

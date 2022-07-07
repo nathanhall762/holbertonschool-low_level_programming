807f14fed082d87415c51b81beee7bc129b98516
nathanhall762 <4943@holbertonschool.com>
2022-07-07 12:59:49 -0700

    inti: task 0 & 1

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+* print_list - prints all the elements of a list_t list
+* @h: pointer to list_t
+*
+* Return: number of elements in a linked list_t list
+*/
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return(0);
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		if (h->str != NULL)
+			printf("[%hu] %s\n", h->len, h->str);
+
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+* list_len - returns the number of elements in a linked list_t list
+* @h: pointer to list_t
+*
+* Return: number of elements in linked list_t list
+*/
+
+size_t list_len(const list_t *h)
+{
+	int count;
+
+	if (h == NULL)
+		return(0);
+
+	else
+	{
+		count = 0;
+		while (h != NULL)
+		{
+			h = h->next;
+			count++;
+		}
+	}
+	return (count);
+}

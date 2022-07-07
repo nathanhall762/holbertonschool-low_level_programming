a515625c67a1d82ff065272f6729801d380141f1
nathanhall762 <4943@holbertonschool.com>
2022-07-07 13:28:27 -0700

    init: task 2 and start task 3

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,10 +1,32 @@
 #include "lists.h"
+#include <string.h>
 
 /**
+* add_node - adds a new node at the beginning of a list_t list
+* @head: address of head node
+* @str: str to add into node
 *
+* Return: the address of the new element of NULL if failed
 */
 
-
+list_t *add_node(list_t **head, const char *str)
 {
-	
+	int i;
+	list_t *new;
+
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+
+	new->len = i + 1;
+	new->str = strdup(str);
+	new->next = *head;
+	*head = new;
+
+	return (new);
 }
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,10 +1,35 @@
 #include "lists.h"
+#include <string.h>
 
 /**
+* add_node_end - adds a new node at the end of a list_t list
+* @head: address of head node
+* @str: str to add into node
 *
+* Return: the address of the new element of NULL if failed
 */
 
-
+list_t *add_node_end(list_t **head, const char *str)
 {
-	
+	int i;
+	list_t *new = *head;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (**head->next != NULL)
+		new = new->next;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+
+	new->len = i + 1;
+	new->str = strdup(str);
+	new->next = *head;
+	*head = new;
+
+	return (new);
 }

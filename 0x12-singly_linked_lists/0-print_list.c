b2fa3e35663504b9fd3c951704d8dd2036926ecb
nathanhall762 <4943@holbertonschool.com>
2022-07-08 09:03:10 -0700

    style: betty task 0 and 1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,7 @@ size_t print_list(const list_t *h)
 	int count = 0;
 
 	if (h == NULL)
-		return(0);
+		return (0);
 
 	while (h != NULL)
 	{
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -12,7 +12,7 @@ size_t list_len(const list_t *h)
 	int count;
 
 	if (h == NULL)
-		return(0);
+		return (0);
 
 	else
 	{

97990854801b634f5c424ce877c313db4727c79a
nathanhall762 <4943@holbertonschool.com>
2022-07-18 13:53:40 -0700

    fix: task 1 added check for null filename

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,6 +13,9 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, i;
 
+	if (filename == NULL)
+		return (-1);
+
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
 	if (fd == -1)
 		return (-1);
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -30,7 +30,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	i = strlen(text_content);
 
-	write(fd, text_content, i);
+	if (text_content != NULL)
+		write(fd, text_content, i);
 
 	close(fd);
 	return (1);

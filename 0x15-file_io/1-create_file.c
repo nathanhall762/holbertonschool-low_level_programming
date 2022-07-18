b2af86d6c43c57164c803609eb3fbaaaa37adc2e
nathanhall762 <4943@holbertonschool.com>
2022-07-18 13:25:45 -0700

    fix: task 0 changed write

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,22 +12,14 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, i;
-	char *buf;
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
 	if (fd == -1)
 		return (-1);
 
-	buf = malloc(sizeof(char) * strlen(text_content));
-	if (buf == NULL)
-	{
-		close(fd);
-		return (-1);
-	}
-
 	i = strlen(text_content);
 
-	write(fd, buf, i);
+	write(fd, text_content, i);
 
 	close(fd);
 	return (1);

55e7c95c4c8fd62fcd549a14f3427fce3b5fb1a7
nathanhall762 <4943@holbertonschool.com>
2022-07-18 13:44:38 -0700

    fix: task 1 if text_content != NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,8 @@ int create_file(const char *filename, char *text_content)
 
 	i = strlen(text_content);
 
-	write(fd, text_content, i);
+	if (text_content != NULL)
+		write(fd, text_content, i);
 
 	close(fd);
 	return (1);

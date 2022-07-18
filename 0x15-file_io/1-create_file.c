913a98d51ddb6addd4e4cb116e567c4f54ea7723
nathanhall762 <4943@holbertonschool.com>
2022-07-18 13:02:20 -0700

    init: task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <string.h>
+
+/*
+* create_file - creates a file
+* @filename: name of the file to created
+* @text_content: NULL terminated string to write to the file
+*
+* Return: 1 on success, -1 on fail
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i;
+	char *buf;
+
+	fd = open(filename, O_CREAT | O_RDWR);
+	if (fd == -1)
+		return (-1);
+
+	buf = malloc(sizeof(char) * strlen(text_content));
+	if (buf == NULL)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	i = strlen(text_content);
+
+	write(fd, buf, i);
+
+	close(fd);
+	return (0);
+}

4f2b8fb91ed1b6143d36aecd6071ffc3cbd7adb0
nathanhall762 <4943@holbertonschool.com>
2022-07-18 13:12:03 -0700

    fix: task 1 file permission mode

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	int fd, i;
 	char *buf;
 
-	fd = open(filename, O_CREAT | O_RDWR);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
 	if (fd == -1)
 		return (-1);
 

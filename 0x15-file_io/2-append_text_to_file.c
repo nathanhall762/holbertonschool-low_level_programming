2865970b0ca897f598822da376ae2a860b331734
nathanhall762 <4943@holbertonschool.com>
2022-07-18 13:17:23 -0700

    init: task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* append_text_to_file - appends text to a file
+* @filename: name of the file to created
+* @text_content: NULL terminated string to write to the file
+*
+* Return: 1 on success, -1 on fail
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, i;
+	char *buf;
+
+	fd = open(filename, O_APPEND | O_RDWR | O_TRUNC, 00600);
+	if (fd == -1)
+		return (-1);
+
+	buf = malloc(sizeof(char) * strlen(text_content));
+	if (buf == NULL)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	i = strlen(text_content);
+
+	write(fd, buf, i);
+
+	close(fd);
+	return (1);
+}

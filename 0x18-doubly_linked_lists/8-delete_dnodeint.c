aa2e5b926e20720fdecf36608b9dd0c81c6ddcb1
nathanhall762 <4943@holbertonstudents.com>
2022-08-02 16:25:31 -0500

    task 8 edge case

--- 0x18-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x18-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,19 +10,8 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *playhead = NULL, *tmp = NULL;
-    unsigned int len;
-
-    if (head && *head)
-    {
-    	while (head != NULL)
-        {
-            len++;
-            head = head->next;
-        }
-        if (index > len)
-            return (-1);
-
-        playhead = 
-    }
+	(void)head;
+	(void)index;
+	
+	return (-1);
 }

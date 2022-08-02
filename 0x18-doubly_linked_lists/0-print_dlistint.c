f5c48da55088f5f65d686c598412affa2e9fba34
nathanhall762 <4943@holbertonstudents.com>
2022-08-02 09:18:14 -0500

    test: task 1 test

--- 0x18-doubly_linked_lists/0-print_dlistint.c
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -1,10 +1,22 @@
 #include "lists.h"
 
 /**
+* print_dlistint - prints all the elements of a list
+* @h: list to print
 *
+* Return: the number of nodes
 */
 
-
+size_t print_dlistint(const dlistint_t *h)
 {
-	
+	int len = 0;
+
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        ++len;
+        h = h->next;
+    }
+
+    return (len);
 }
